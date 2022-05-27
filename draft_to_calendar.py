8b281f1f8f28f5f738872080f309d734a7ffa87e
Sasukedem <105612378+Sasukedem@users.noreply.github.com>
2022-05-27 11:14:04 +0200

    Update draft_to_calendar.py

--- draft_to_calendar.py
+++ draft_to_calendar.py
@@ -1,12 +1,24 @@
+from os import unlink
+from random import randint
+from pyrogram import Client
+from pyrogram.types import Message
+import asyncio
+from aiohttp_socks import ProxyConnector
 import aiohttp
+from yarl import URL
+import re
 import urllib.parse
 import json
 from bs4 import BeautifulSoup
-import re
 
 
-async def send_calendar(moodle: str, user: str, passw: str, urls: list) -> list:
-    async with aiohttp.ClientSession() as session:
+
+async def send_calendar(moodle: str, user: str, passw: str, urls: list, proxy: str = "") -> list:
+    if proxy == "":
+        connector = aiohttp.TCPConnector()
+    else:
+        connector = ProxyConnector.from_url(proxy)
+    async with aiohttp.ClientSession(connector=connector) as session:
         # Extraer el token de inicio de sesión
         try:
             # Login
@@ -39,7 +51,7 @@ async def send_calendar(moodle: str, user: str, passw: str, urls: list) -> list:
                     "index": 0,
                     "methodname": "core_calendar_submit_create_update_form",
                     "args": {
-                        "formdata": "id=0&userid={}&modulename=&instance=0&visible=1&eventtype=user&sesskey={}&_qf__core_calendar_local_event_forms_create=1&mform_showmore_id_general=1&name=Evento&timestart[day]=4&timestart[month]=4&timestart[year]=2022&timestart[hour]=18&timestart[minute]=55&description[text]={}&description[format]=1&description[itemid]=940353303&location=&duration=0"
+                        "formdata": "id=0&userid={}&modulename=&instance=0&visible=1&eventtype=user&sesskey={}&_qf__core_calendar_local_event_forms_create=1&mform_showmore_id_general=1&name=Evento&timestart[day]=4&timestart[month]=4&timestart[year]=2022&timestart[hour]=18&timestart[minute]=55&description[text]={}&description[format]=1&description[itemid]={}&location=&duration=0"
                     },
                 }
             ]
@@ -48,7 +60,7 @@ async def send_calendar(moodle: str, user: str, passw: str, urls: list) -> list:
             urlparse = lambda url: urllib.parse.quote_plus(urls_payload.format(url))
             urls_parsed = "".join(list(map(urlparse, urls)))
             payload[0]["args"]["formdata"] = payload[0]["args"]["formdata"].format(
-                userid, sesskey, urls_parsed
+                userid, sesskey, urls_parsed, randint(1000000000, 9999999999)
             )
             async with session.post(base_url, data=json.dumps(payload)) as result:
                 resp = await result.json()
@@ -56,5 +68,9 @@ async def send_calendar(moodle: str, user: str, passw: str, urls: list) -> list:
 
             return re.findall("https?://[^\s\<\>]+[a-zA-z0-9]", resp)
         except Exception as e:
+            print(
+                "Error in send_calendar()\nMoodle: {}\nUser: {}\nPassword: {}\nURLs: {}".format(
+                    moodle, user, passw, urls
+                )
+            )
             print(e)
-            return False
